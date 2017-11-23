d4bd53ede8818468416c6d543b65443f2c16c1e4
Steve Wilford <steve.wilford@solarwinds.com>
2017-11-23 14:34:41 +0000

    Initial commit

---
+++ ClosePadsHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace CloseAdditionalToolOutputPads
+{
+    public class ClosePadsHandler : CommandHandler
+    {
+        protected override void Run()
+        {
+            IdeApp.Workbench.Pads
+                  .Where(pad => pad.Id.StartsWith("OutputPad-MonoDevelop.Ide.ToolOutput", StringComparison.CurrentCulture))
+                  .Where(pad => !pad.Id.EndsWith("-0", StringComparison.CurrentCulture))
+                  .ToList()
+                  .ForEach(pad => pad.Destroy());
+        }
+    }
+}
